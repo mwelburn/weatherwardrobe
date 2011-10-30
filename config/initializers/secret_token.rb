3814a1fca9a22c66fba4abb00d11955e1ef57a3b
Michael Welburn <mwelbu2@gmail.com>
2011-10-30 09:35:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weatherwardrobe::Application.config.secret_token = 'b3cce00cc3817a6f08d5de1efc71238ece67557e3dd68b56a71e6ec1db146fc8a4833933e01e72df6ff75c9916f7dd68f66b6098471ac310bfcb571d9a4edbec'
