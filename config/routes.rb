04f7f67f3f0b1bdd291c59cc3368bbcc54ba82a7
Michael Welburn <mwelbu2@gmail.com>
2011-10-31 21:00:42 -0500

    devise views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Weatherwardrobe::Application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
   devise_scope :user do
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
   end
+
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
