f9c3a11e5f7568ebbc1ceb205e3a1949613e5c89
br0nc080 <mwelbu2@gmail.com>
2011-11-04 00:07:53 -0500

    updating with facebook_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :gender, :password, :password_confirmation, :remember_me
+  attr_accessible :facebook_id, :name, :email, :gender, :password, :password_confirmation, :remember_me
 
   validates :gender, :inclusion => { :in => %w(M F) }
+  validates_uniqueness_of :facebook_id
+
+  # Remove this if we ever allow non-Facebook access
+  validates_presence_of :facebook_id
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token['extra']['user_hash']
-    if user = User.find_by_email(data["email"])
+    if user = User.find_by_facebook_id(data["id"])
+      # TODO - should we check if we need to update any information here?
       user
     else # Create a user with a stub password.
       # TODO - What do we do if this is empty...
@@ -19,13 +24,14 @@ class User < ActiveRecord::Base
       if !data["gender"].nil?
         gender = data["gender"][0].upcase
       end
-      User.create(:email => data["email"], :name => data["name"], :gender => gender, :password => Devise.friendly_token[0,20])
+      User.create(:facebook_id => data["id"], :email => data["email"], :name => data["name"], :gender => gender, :password => Devise.friendly_token[0,20])
     end
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
+        user.facebook_id = data["facebook_id"]
         user.email = data["email"]
         user.name = data["name"]
         user.gender = data["gender"]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111104032007) do
+ActiveRecord::Schema.define(:version => 20111104044934) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20111104032007) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "gender"
+    t.string   "facebook_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
