731d79b39787e4d170602920bde7ef56ce6a9910
br0nc080 <mwelbu2@gmail.com>
2011-11-04 00:22:11 -0500

    forget precompiling assets for now...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Weatherwardrobe::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+# TODO - set this back to false and precompile myself if this ever goes live
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
